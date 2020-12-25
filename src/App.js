b4481813daaacc0f250924b567f36d88acf749cf
Jun Naruse <jun.naruse@pucp.edu.pe>
2020-12-25 18:38:03 -0500

    Delete some js componentes
    Delete initial components such as test, because we are not going to use them. Clean of some initial code as well

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Patient's manager</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <Form />
+          </div>
+          <div className="col-12 col-md-6">
+            2
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
